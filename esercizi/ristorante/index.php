0320f9ac0c0d5dccdf531a0c930c8ff5f4102e2a
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-11-13 20:14:43 +0000

    Intestazione e colori

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -11,14 +11,22 @@
  $secondi = array("Arrosto di carne", "Grigliata di manzo", " Peperoni ripieni");
  $contorni = array("Patate al forno", "Fagioli in salsa", "Verdura cotta");
  $dolci = array("Crostata alle prugne", "Cheesecake al caramello", "Pavlova alle fragole");
-?>   
-<nav class="navbar bg-body-tertiary">
+?> 
+<body style="background-color: #dfbbb1;"> 
+<nav class="navbar" style="background-color:#456990">
   <div class="container-fluid">
-    <a class="navbar-brand" href="https://www.giallozafferano.it/">
+    <a class="navbar-brand  text-white" href="https://www.giallozafferano.it/">
       <img src="https://play-lh.googleusercontent.com/DdibcNOAQ6_aDICit1J0bQQbwEe9kgmP6Pr2txzORaHs4vRb8KS43f_ysuygPWERzw" alt="Logo" width="30" class="d-inline-block align-text-top">
       Giallo Zafferano
     </a>
   </div>
 </nav>
+<div class="mt-4 p-5 text-white rounded" style="text-align:center; background-color: #49beaa;">
+        <img src="https://www.giallozafferano.it/style/images/logo-giallozafferano.svg">
+        <h1 style="font-weight: bolder; font-size: xxx-large;">Ristorante Giallo Zafferano</h1>
+        <h2>Piatti da favola</h2>
+</div>
+<br>
+</body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
