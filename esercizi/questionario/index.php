f3f8da8c71eb7bd2c1b807ebab6263c638c27c49
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-11-30 15:41:56 +0000

    form

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -8,24 +8,40 @@
 </head>
 <body>
     <form action="./accesso.php">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"><br>
-        <label for="cognome">Cognome:</label>
-        <input type="text" id="cognome" name="cognome"><br>
-        <label for="age">Età:</label>
-        <input type="number" id="age" name="age"><br>
-        <label for="sport">Sport preferito:</label>
-        <input type="text" id="sport" name="sport"><br>
-        <label for="colore">Inserire colore sfondo:</label>
-        <input type="color" id="colore" name="colore"><br>
-        <?php 
-        do{ 
-        "<label for='password'>Password:</label>
-        <input type='password' id='password' name='password'><br>";
-        }
-        while($_GET['password']=='info2023');
-        ?>
-        <input type="submit">
+        <div class="row g-3">
+            <div class="col">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome"><br>
+            </div>
+            <div class="col">
+            <label for="cognome">Cognome:</label>
+            <input type="text" id="cognome" name="cognome"><br>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email"><br>
+        </div>
+        <div class="col-md-6">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <div class="col-md-6">
+            <label for="sport">Sport preferito:</label>
+            <input type="text" id="sport" name="sport"><br>
+        </div>
+        <div class="col-md-4">
+            <label for="age">Età:</label>
+            <input type="number" id="age" name="age"><br>  
+        </div>
+        <div class="col-md-2">
+            <label for="colore">Inserire colore sfondo:</label>
+            <input type="color" id="colore" name="colore"><br>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Accedi</button>
+        </div>
     </form>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
