b79fe7a4de277e4d69aa4136f415aa692620c72c
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-12-02 11:57:52 +0000

    grafica 3

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -6,7 +6,7 @@
     <title>Questionario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body style="background-color:darkslategray; font-family:Cambria; font-size:larger">
+<body style="background-color:darkslategray; font-family:serif; font-size:larger">
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Questionario
@@ -34,6 +34,7 @@
     <table  style="margin: 20px;">
     <td style="background-color:lavender; border-radius:10px">
     <center>
+      <br>
         <h1>Questionario d'accesso</h1>
         <br>
     <form action="./accesso.php">
