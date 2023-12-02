a0c433a7480119acdd037963a1ee2dc8f836499b
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-12-02 14:28:17 +0000

    Grafica 4

--- esercizi/questionario/accesso.php
+++ esercizi/questionario/accesso.php
@@ -31,51 +31,57 @@
   </div>
 </nav>
 <br>
+<?php 
+$password = "info2023";
+$tentativi = 3;
+while($tentativi > 0){
+if($_GET['password']==$password){
+?>
 <center>
-    <h1>Benvenuto <?= $_GET['nome']?> <?= $_GET['cognome'] ?> </h1>
+    <h1>Benvenuto <?=$_GET['nome']?> <?=$_GET['cognome']?> </h1>
 </center>
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-lg-6 mb-4 mb-lg-0">
-        <div class="card mb-3" style="border-radius: .5rem;">
-          <div class="row g-0">
-            <div class="col-md-4 gradient-custom text-center " style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-              <img src="https://icleonardodavincisenago.edu.it/assets/avatar/141319A3-7592-4997-970CD777CE3FE2F8/"
-                alt="Avatar" class="img-fluid my-5" style="width: 100px;" />
-              <h5><?= $_GET['nome']?> <?= $_GET['cognome'] ?></h5>
+  <div class='container py-5 h-100'>
+    <div class='row d-flex justify-content-center align-items-center h-100'>
+      <div class='col col-lg-6 mb-4 mb-lg-0'>
+        <div class='card mb-3' style='border-radius: .5rem;'>
+          <div class='row g-0'>
+            <div class='col-md-4 gradient-custom text-center ' style='border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;'>
+              <img src='https://icleonardodavincisenago.edu.it/assets/avatar/141319A3-7592-4997-970CD777CE3FE2F8/'
+                alt='Avatar' class='img-fluid my-5' style='width: 100px;' />
+              <h5><?=$_GET['nome']?> <?=$_GET['cognome']?></h5>
               <p>Utente</p>
-              <i class="far fa-edit mb-5"></i>
+              <i class='far fa-edit mb-5'></i>
             </div>
-            <div class="col-md-8">
-              <div class="card-body p-4">
+            <div class='col-md-8'>
+              <div class='card-body p-4'>
                 <h6>Informazioni</h6>
-                <hr class="mt-0 mb-4">
-                <div class="row pt-1">
-                  <div class="col-6 mb-3">
+                <hr class='mt-0 mb-4'>
+                <div class='row pt-1'>
+                  <div class='col-6 mb-3'>
                     <h6>Email</h6>
-                    <p class="text-muted"><?= $_GET['email']?></p>
+                    <p class='text-muted'><?=$_GET['email']?></p>
                   </div>
-                  <div class="col-6 mb-3">
+                  <div class='col-6 mb-3'>
                     <h6>Sport preferito</h6>
-                    <p class="text-muted"><?= $_GET['sport']?></p>
+                    <p class='text-muted'><?=$_GET['sport']?></p>
                   </div>
                 </div>
                 <h6>Altro</h6>
-                <hr class="mt-0 mb-4">
-                <div class="row pt-1">
-                  <div class="col-6 mb-3">
+                <hr class='mt-0 mb-4'>
+                <div class='row pt-1'>
+                  <div class='col-6 mb-3'>
                     <h6>Password</h6>
-                    <p class="text-muted"><?= $_GET['password']?></p>
+                    <p class='text-muted'><?=$_GET['password']?></p>
                   </div>
-                  <div class="col-6 mb-3">
+                  <div class='col-6 mb-3'>
                     <h6>Età</h6>
-                    <p class="text-muted"><?= $_GET['age']?></p>
+                    <p class='text-muted'><?=$_GET['age']?></p>
                   </div>
                 </div>
-                <div class="d-flex justify-content-start">
-                  <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
-                  <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
-                  <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                <div class='d-flex justify-content-start'>
+                  <a href='#!'><i class='fab fa-facebook-f fa-lg me-3'></i></a>
+                  <a href='#!'><i class='fab fa-twitter fa-lg me-3'></i></a>
+                  <a href='#!'><i class='fab fa-instagram fa-lg'></i></a>
                 </div>
               </div>
             </div>
@@ -84,8 +90,28 @@
       </div>
     </div>
   </div>
+  <?php
+$tentativi=0;
+}
+else{
+  $tentativi=3;
+?>
+  <p>La password è errata <?=$tentativi?> rimasti
+  Inserisci nuovamente la password </p>
+  <br>
+  <label for='password'>Password:</label>
+  <input type='password' id='password' name='password'><br>
 
-    <footer class="footer mt-auto py-3" style="background-color: black; align:">
+<?php
+  if($_GET['password']==$password){
+    $tentativi=0;
+  }
+
+  $tentativi--;
+}
+}
+?>
+    <footer class="footer mt-auto py-3" style="background-color: black;">
         <div class="container">
           <span class="text-light">
           <div style="width: 30%; align:left"><img src="https://static.vecteezy.com/system/resources/previews/024/029/746/non_2x/book-icon-clipart-transparent-background-free-png.png" style="width: 60px"></div>
