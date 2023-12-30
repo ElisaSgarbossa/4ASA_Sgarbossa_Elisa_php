485d8fc8af8cdb0f88acef3be2a0a3fecfbe83dd
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-12-30 20:45:52 +0000

    parte 2

--- esercizi/monitoraggio/temp.php
+++ esercizi/monitoraggio/temp.php
@@ -2,26 +2,18 @@
 <title>Monitoraggio con ESP32</title>
 <?php
 //trasformiamo la stringa in un array
-//$dati = json_decode(file_get_contents('raccolta_dati.txt')); //i dati vengono presi da un file di testo (dove la scheda li aveva salvati)
-$dati = array(array("temp" => "0.00", "hum" => "0.00", "date" => "2023-12-05 09:33:47"), array("temp" => "4.00", "hum" => "79.00", "date" => "2023-12-05 09:34:02"),array("temp" => "10.00", "hum" => "38.00", "date" => "2023-12-05 09:34:09"),array("temp" => "7.00", "hum" => "7.00", "date" => "2023-12-05 09:34:16"),array("temp" => "4.00", "hum" => "96.00", "date" => "2023-12-05 09:34:23"),array("temp" => "19.00", "hum" => "118.00", "date" => "2023-12-05 09:34:31"));
+$dati = json_decode(file_get_contents('raccolta_dati.txt')); //i dati vengono presi da un file di testo (dove la scheda li aveva salvati)
 ?>
+<center>
+<h1 style="font-family:sans-serif">Grafico monitoraggio umidità e temperatura</h1>
+</center>
 <div>
   <canvas id="myChart"></canvas>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
   const ctx = document.getElementById('myChart');
-  <?php
-            foreach ($dati as $val => $array) {
-                echo "Chiave esterna: $val <br>";
-            
-                foreach ($array as $item => $valore) {
-                    echo "La chiave è: $item, il valore è: $valore <br>";
-                }
-            }
-            ?>
   new Chart(ctx, {
     type: 'line',
     data: {
@@ -31,14 +23,16 @@ $dati = array(array("temp" => "0.00", "hum" => "0.00", "date" => "2023-12-05 09:
                 data: [65, 59, 80, 81, 56, 55, 40],
                 fill: false,
                 borderColor: 'rgb(75, 192, 192)',
-                tension: 0.3
+                tension: 0.4,
+                borderWidth: 4.4
             },
             {
                 label: 'Umidità',
                 data: [43, 99, 23, 71, 109, 49, 30],
                 fill: false,
-                borderColor: 'rgb(45, 218, 92)',
-                tension: 0.3
+                borderColor: 'rgb(145, 78, 92)',
+                tension: 0.4,
+                borderWidth: 4.4
             }]
             },
     options: {
