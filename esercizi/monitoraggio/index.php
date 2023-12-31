fd5f96acb68c8fd4079b8dc8c8e61de03119c1cf
ElisaSgarbossa <147228806+ElisaSgarbossa@users.noreply.github.com>
2023-12-31 13:32:05 +0000

    Mod

--- esercizi/monitoraggio/index.php
+++ esercizi/monitoraggio/index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
+<html>
+<body>
+<div>   <canvas id="myChart"></canvas> </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+const ctx = document.getElementById('myChart');
 
-<?php
-            foreach ($dati as $val => $array) {
-                echo "Chiave esterna: $val <br>";
-            
-                foreach ($array as $item => $valore) {
-                    echo "La chiave è: $item, il valore è: $valore <br>";
-                }
-            }
-            ?>
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [1, 2, 3, 4, 5, 6],
+      datasets: [{
+        label: 'temperatura',
+        data: [18.7, 17.1, 18.9, 16.0, 14.2, 20.8],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+</script>
+</body>
 </html>
